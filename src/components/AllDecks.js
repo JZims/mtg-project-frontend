13f05ba8c17d7539fd2a0ceda2ef6f4061bdb630
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-05-11 11:55:14 -0400

    Initial database load setup

--- src/components/AllDecks.js
+++ src/components/AllDecks.js
@@ -1,7 +1,26 @@
 import DeckCard from "./DeckCard";
-
+import {useState, useEffect} from "react"
 
 function AllDecks() {
+    const [decksArray, setDecksArray] = useState([])
+
+    // Initial fetch to server for decks
+    useEffect(() => {
+        fetch("http://localhost:9292/decks")
+            .then(resp => resp.json())
+            .then(function(deckServerData) {
+                setDecksArray(deckServerData)
+            })
+    }, [])
+    // console.log(decksArray)
+    const deckCardArray = decksArray.map(function(deck) {
+        return <DeckCard 
+                key={deck.id}
+                id={deck.id}
+                deckName={deck.name}
+                imgUrl={deck.img_url}
+        />
+    })
 
     return (
         <div className="page" id="all-decks">
@@ -10,10 +29,11 @@ function AllDecks() {
                 <option value="Available">Available to Rent</option>
             </select>
             <div id="all-decks-list">
+                {deckCardArray}
+                {/* <DeckCard />
                 <DeckCard />
                 <DeckCard />
-                <DeckCard />
-                <DeckCard />
+                <DeckCard /> */}
             </div>
         </div>
     )
